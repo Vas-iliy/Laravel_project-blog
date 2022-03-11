f5fcdd4e9d50f0a19559d3c15afe533c5f78f8a5
Vasiliy <kkolyasev1228@gmail.com>
2022-03-11 11:56:10 +0400

    migration

--- database/migrations/2022_03_11_074604_create_post_tag_table.php
+++ database/migrations/2022_03_11_074604_create_post_tag_table.php
@@ -14,8 +14,9 @@ class CreatePostTagTable extends Migration
     public function up()
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
         });
     }
 
