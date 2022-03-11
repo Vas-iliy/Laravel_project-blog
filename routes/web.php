f66bfbf7817afbaeca8bbdf56d27f27ae7433c1d
Vasiliy <kkolyasev1228@gmail.com>
2022-03-11 17:42:29 +0400

    start frontend

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', 'PostController@index')->name('home');
+Route::get('/article', 'PostController@show')->name('posts.article');
 
 //Admin
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
