44d7f255ccf48d8e832c3c5d4948279d4e463d1d
Vasiliy <kkolyasev1228@gmail.com>
2022-03-11 15:15:09 +0400

    end crud post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePost;
 use App\Post;
 use App\Tag;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -28,10 +29,7 @@ class PostController extends Controller
     {
         $data = $request->all();
 
-        if ($request->hasFile('image')) {
-            $folder = date('Y-m-d');
-            $data['image'] = $request->file('image')->store("images/{$folder}");
-        }
+        $data['image'] = Post::uploadImage($request);
 
         $post = Post::query()->create($data);
         $post->tags()->sync($request->tags);
@@ -40,19 +38,28 @@ class PostController extends Controller
 
     public function edit($id)
     {
-        $category = Category::query()->find($id);
-        return view('admin.categories.edit', compact('category'));
+        $categories = Category::query()->pluck('title', 'id')->all();
+        $tags = Tag::query()->pluck('title', 'id')->all();
+        $post = Post::query()->find($id);
+        return view('admin.posts.edit', compact('categories', 'tags', 'post'));
     }
 
     public function update(StorePost $request, $id)
     {
-
-        return redirect()->route('categories.index')->with('success', 'Изменения сохранены');
+        $post = Post::query()->find($id);
+        $data = $request->all();
+        $data['image'] = Post::uploadImage($request, $post->image);
+        $post->update($data);
+        $post->tags()->sync($request->tags);
+        return redirect()->route('posts.index')->with('success', 'Изменения сохранены');
     }
 
     public function destroy($id)
     {
-
-        return redirect()->route('categories.index')->with('success', 'Статья удалена');
+        $post = Post::query()->find($id);
+        $post->tags()->sync([]);
+        Storage::delete($post->image);
+        $post->delete();
+        return redirect()->route('posts.index')->with('success', 'Статья удалена');
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -28,5 +30,25 @@ class Post extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public static function uploadImage(Request $request, $image = null)
+    {
+        if ($request->hasFile('image')) {
+            if ($image) {
+                Storage::delete($image);
+            }
+            $folder = date('Y-m-d');
+            return $request->file('image')->store("images/{$folder}");
+        }
+        return null;
+    }
+
+    public function getImage()
+    {
+        if (!$this->image) {
+            return null;
+        }
+        return asset('uploads/'.$this->image);
     }
 }
