ca24e47b80d6b89216e6c3c2d3da400981e85d13
Vasiliy <kkolyasev1228@gmail.com>
2022-03-11 13:40:02 +0400

    crud tags

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTag;
+use App\Tag;
+
+class TagController extends Controller
+{
+
+    public function index()
+    {
+        $tags = Tag::query()->paginate(20);
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    public function store(StoreTag $request)
+    {
+        Tag::query()->create($request->all());
+        return redirect()->route('tags.index')->with('success', 'Тег добавлен');
+    }
+
+    public function edit($id)
+    {
+        $tag = Tag::query()->find($id);
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    public function update(StoreTag $request, $id)
+    {
+        Tag::query()->find($id)->update($request->all());
+        return redirect()->route('tags.index')->with('success', 'Изменения сохранены');
+    }
+
+    public function destroy($id)
+    {
+        Tag::destroy($id);
+        return redirect()->route('tags.index')->with('success', 'Тег удален');
+    }
+}
