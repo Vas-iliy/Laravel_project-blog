a5efc6091042c62d683d37581be0b7cf8d11582f
Vasiliy <kkolyasev1228@gmail.com>
2022-03-14 12:19:19 +0400

    sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Category;
+use App\Post;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer(['layouts.layout', 'layouts.category-layout'], function ($view) {
             $view->with('categories', Category::all());
+        });
+        view()->composer('layouts.sidebar', function ($view) {
+           $view->with('popular_posts', Post::query()->orderBy('views', 'desc')->limit(3)->get());
+           $view->with('cats', Category::query()->withCount('posts')->orderBy('posts_count', 'desc')->get());
         });
     }
 }
